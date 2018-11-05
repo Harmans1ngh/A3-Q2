c3b80e61cce361ad32269c7c3110d01a2decc3ac
Harmans1ngh <Harmans1ngh@users.noreply.github.com>
2018-11-04 23:40:35 -0500

    added files

---
+++ EchoClient.java
@@ -0,0 +1,43 @@
+/*
+Assignment 2: Question #1
+Bikramjit Garcha (991330905)
+Harmanjeet Singh (991439186)
+*/
+
+
+import java.net.*;
+import java.io.*;
+
+public class EchoClient
+{
+	public static void main(String[] args) {
+		try {
+			/* make connection to server socket */
+			Socket sock = new Socket("127.0.0.1",6013);
+
+			BufferedReader read = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			BufferedReader connection = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter writer = new PrintWriter(sock.getOutputStream(), true); 
+			
+
+			/* Reads user input from the client and stores it into PrintWriter which than sends it
+			   to the OutputStream which is than printed by the client */
+
+			String input;
+
+			do {
+
+				input = read.readLine();
+				if ( input != null )
+					System.out.println(input);
+				input = connection.readLine();
+				writer.println(input);
+			}
+			while (!input.trim().equals("."));
+			/* close the socket connection*/
+		}		
+		catch (IOException ioe) {
+		System.err.println(ioe);
+		}
+	}
+}
---
+++ EchoServer.java
@@ -0,0 +1,45 @@
+/*
+Assignment 2: Question #1
+Bikramjit Garcha (991330905)
+Harmanjeet Singh (991439186)
+*/
+
+import java.net.*;
+import java.io.*;
+public class EchoServer
+{
+	public static void main(String[] args) {
+		try {
+			ServerSocket sock = new ServerSocket(6013); //creates new socket at the specified portnumber 
+			
+			/* listens for connections */
+
+			while (true) {
+
+				Socket client = sock.accept();
+				System.out.println("connection opened");
+
+				BufferedReader read = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				PrintWriter writer = new PrintWriter(client.getOutputStream(), true);  
+				writer.println("welcome to the Echoserver. Type '.' to end ");
+				
+				/* Gets user input from the client and stores it into PrintWriter which than sends it
+			           to the OutputStream */
+
+				String input;
+				do {
+					input = read.readLine();
+					if ( input != null )
+						writer.println("Server:" + input);
+				}
+				while (!input.trim().equals(".")); // close the socket when '.' is entered
+
+				System.out.println("connection closed");
+				client.close();
+			}
+		}
+		catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
+}
